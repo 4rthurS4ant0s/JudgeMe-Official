85fb1d00d01b441f5a946723cfe00d3b86b34c27
Arthur Santos <contato.arthursgc@gmail.com>
2018-01-25 20:23:15 -0200

    Activity da primeira etapa do processo de cadastro editado e funcionando.

--- app/src/main/java/comj/example/android/judgeme_androidapp/Activities/CreateAccount1Activity.java
+++ app/src/main/java/comj/example/android/judgeme_androidapp/Activities/CreateAccount1Activity.java
@@ -1,15 +1,169 @@
 package comj.example.android.judgeme_androidapp.Activities;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.util.Calendar;
 
 import comj.example.android.judgeme_androidapp.R;
 
-public class CreateAccount1Activity extends AppCompatActivity {
+public class CreateAccount1Activity extends Activity {
+
+    private Spinner spinnerGeneros;
+    private ArrayAdapter arrayAdapter;
+
+    private EditText editTextNome;
+    private EditText editTextSobrenome;
+    private EditText editTextNascimento;
+
+    private TextView textViewCancelar;
+
+    private Button buttonAvancar;
+
+    private String nome, sobrenome, nomeCompleto, genero, nascimento;
+
+    private int pYear;
+    private int pMonth;
+    private int pDay;
+    /** This integer will uniquely define the dialog to be used for displaying date picker.*/
+    static final int DATE_DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account1);
+
+        spinnerGeneros = findViewById(R.id.spinnerCreateAccount1Generos);
+        arrayAdapter = ArrayAdapter.createFromResource(this, R.array.hint_generos, R.layout.spinner_items);
+        spinnerGeneros.setAdapter(arrayAdapter);
+
+        editTextNascimento = findViewById(R.id.editTextCreateAccount1Nascimento);
+        /**-----------MASCARANDO O FORMATO DA EDITTEXT DA DATA DE NASCIMENTO-------------**/
+        SimpleMaskFormatter smf = new SimpleMaskFormatter("NN/NN/NNNN");
+        MaskTextWatcher mtw = new MaskTextWatcher(editTextNascimento, smf);
+        editTextNascimento.addTextChangedListener(mtw);
+        /**-----------MASCARANDO O FORMATO DA EDITTEXT DA DATA DE NASCIMENTO-------------**/
+
+        /**-----------------------------MOSTRANDO O CALENDÁRIO PARA QUE O USUÁRIO POSSA SELECIONAR O DIA DO NASCIMENTO--------------------------------------**/
+        editTextNascimento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        /** Get the current date */
+        final Calendar cal = Calendar.getInstance();
+        pYear = cal.get(Calendar.YEAR);
+        pMonth = cal.get(Calendar.MONTH);
+        pDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        /** Display the current date in the TextView */
+        updateDisplay();
+        /**-----------------------------MOSTRANDO O CALENDÁRIO PARA QUE O USUÁRIO POSSA SELECIONAR O DIA DO NASCIMENTO--------------------------------------**/
+
+        editTextNome = findViewById(R.id.editTextCreateAccount1PrimeiroNome);
+        editTextSobrenome = findViewById(R.id.editTextCreateAccount1Sobrenome);
+
+        textViewCancelar = findViewById(R.id.textViewCreateAccountStep1Cancel);
+        textViewCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CreateAccount1Activity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        buttonAvancar = findViewById(R.id.buttomCreateAccount1Avancar);
+        buttonAvancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nome = editTextNome.getText().toString();
+                sobrenome = editTextSobrenome.getText().toString();
+
+                nome = nome.trim();
+                sobrenome = sobrenome.trim();
+
+                genero = spinnerGeneros.getSelectedItem().toString();
+                nascimento = editTextNascimento.getText().toString();
+
+                if(nome.isEmpty() || sobrenome.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Nome ou sobrenome incompleto(s)",Toast.LENGTH_SHORT).show();
+                }else{
+                    nomeCompleto = nome+" "+sobrenome;
+                    Toast.makeText(getApplicationContext(), "Avançar",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
+
+    /**-----------------------------MOSTRANDO O CALENDÁRIO PARA QUE O USUÁRIO POSSA SELECIONAR O DIA DO NASCIMENTO--------------------------------------**/
+    /** Callback received when the user "picks" a date in the dialog */
+    private DatePickerDialog.OnDateSetListener pDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+
+                public void onDateSet(DatePicker view, int year,
+                                      int monthOfYear, int dayOfMonth) {
+                    pYear = year;
+                    pMonth = monthOfYear;
+                    pDay = dayOfMonth;
+                    updateDisplay();
+                }
+            };
+
+    /** Updates the date in the TextView */
+    private void updateDisplay() {
+
+        String day = String.valueOf(pDay);
+        String month = String.valueOf(pMonth+1);
+        String year = String.valueOf(pYear);
+        if(pDay<10){
+            day = "0"+day;
+        }
+        if(pMonth+1<10){
+            month = "0"+month;
+        }
+        editTextNascimento.setText(
+                new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(day).append("/")
+                        .append(month).append("/")
+                        .append(year).append(" "));
+
+    }
+
+    /** Create a new dialog for date picker */
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this,
+                        pDateSetListener,
+                        pYear, pMonth, pDay);
+        }
+        return null;
+    }
+    /**-----------------------------MOSTRANDO O CALENDÁRIO PARA QUE O USUÁRIO POSSA SELECIONAR O DIA DO NASCIMENTO--------------------------------------**/
+
 }
--- app/src/main/java/comj/example/android/judgeme_androidapp/Activities/LoginActivity.java
+++ app/src/main/java/comj/example/android/judgeme_androidapp/Activities/LoginActivity.java
@@ -1,9 +1,11 @@
 package comj.example.android.judgeme_androidapp.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.nio.channels.AcceptPendingException;
 
@@ -11,12 +13,27 @@ import comj.example.android.judgeme_androidapp.R;
 
 public class LoginActivity extends Activity {
 
+    private TextView tvEsqueceuSenha;
+    private TextView tvLoginCriarConta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        tvEsqueceuSenha = findViewById(R.id.textViewLoginEsqueceuSenha);
+        tvLoginCriarConta = findViewById(R.id.textViewLoginCriarConta);
 
+        tvLoginCriarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, CreateAccount1Activity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
+
 }
