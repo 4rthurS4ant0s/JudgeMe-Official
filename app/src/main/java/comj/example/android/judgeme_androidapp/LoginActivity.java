6cd015ef9fd7e5d88755c0223c336b8f93c746b8
Arthur Santos <contato.arthursgc@gmail.com>
2018-01-25 00:06:04 -0200

    Design da activity Login concluído.

---
+++ app/src/main/java/comj/example/android/judgeme_androidapp/LoginActivity.java
@@ -0,0 +1,20 @@
+package comj.example.android.judgeme_androidapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.nio.channels.AcceptPendingException;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
