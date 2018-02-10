117403ab8955a99daac9d96ca5d5c81896ed0288
Arthur Santos <contato.arthursgc@gmail.com>
2018-02-10 15:06:12 -0200

    SearchList está mostrando todos os usuários.

--- app/src/main/java/comj/example/android/judgeme_androidapp/Adapters/CustomListViewSearch.java
+++ app/src/main/java/comj/example/android/judgeme_androidapp/Adapters/CustomListViewSearch.java
@@ -1,15 +1,24 @@
 package comj.example.android.judgeme_androidapp.Adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import comj.example.android.judgeme_androidapp.Helpers.Base64Custom;
 import comj.example.android.judgeme_androidapp.R;
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,6 +31,9 @@ public class CustomListViewSearch extends BaseAdapter {
     private Context mContext;
     private ArrayList<HashMap<String, String>> usuarioSearch;
     private static LayoutInflater inflater = null;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public CustomListViewSearch(Context context, ArrayList<HashMap<String, String>> data) {
 
@@ -52,7 +64,7 @@ public class CustomListViewSearch extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
         final View view = inflater.inflate(R.layout.list_search, null);
 
@@ -61,10 +73,42 @@ public class CustomListViewSearch extends BaseAdapter {
         mSearchData = usuarioSearch.get(position);
 
         CircleImageView circleImageView = view.findViewById(R.id.circleImageViewListSearchFotoPerfil);
-        TextView textViewNome = view.findViewById(R.id.textViewListSearchNomeUsuario);
-        TextView textViewNick = view.findViewById(R.id.textViewListSearchNickUsuario);
+        final TextView textViewNome = view.findViewById(R.id.textViewListSearchNomeUsuario);
+        final TextView textViewNick = view.findViewById(R.id.textViewListSearchNickUsuario);
 
-        textViewNome.setText(mSearchData.get("nome"));
+        db.collection("searchListEmails")
+                .document("usuarios")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                int newPosition = position;
+                if(newPosition > Integer.parseInt(mSearchData.get("qtd"))){
+                    newPosition = Integer.parseInt(mSearchData.get("qtd"));
+                }else{
+                    newPosition += 1;
+                }
+
+                String email = task.getResult().getString(String.valueOf(newPosition));
+
+                //Log.d("emails",String.valueOf(newPosition));
+                //Log.d("emails",task.getResult().getString(String.valueOf(newPosition)));
+                db.collection("usuarios")
+                        .document(Base64Custom.converterBase64(email))
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        textViewNome.setText(task.getResult().getString("nome_completo"));
+                        textViewNick.setText(task.getResult().getString("nickname"));
+
+                    }
+                });
+
+            }
+        });
 
         return view;
     }
--- app/src/main/java/comj/example/android/judgeme_androidapp/Fragments/SearchFragment.java
+++ app/src/main/java/comj/example/android/judgeme_androidapp/Fragments/SearchFragment.java
@@ -2,11 +2,18 @@ package comj.example.android.judgeme_androidapp.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,22 +38,38 @@ public class SearchFragment extends Fragment {
     private ListView listView;
     private CustomListViewSearch customListViewSearch;
 
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        final View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         searchList = new ArrayList<>();
         data = new HashMap<>();
-        for(int i = 0; i < 10; i++) {
-            data.put("nome", "arthur");
-            searchList.add(data);
-        }
-        listView =  view.findViewById(R.id.listViewSearch);
 
-        customListViewSearch = new CustomListViewSearch(getContext(), searchList);
-        listView.setAdapter(customListViewSearch);
+        db.collection("autoIncrementSearchListEmails")
+                .document("usuarios")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                int qtd = Integer.parseInt(task.getResult().getString("quantidade"));
+
+                for(int i = 1; i < qtd ; i++) {
+                    data.put("qtd", String.valueOf(qtd - 1));
+                    searchList.add(data);
+                }
+                listView =  view.findViewById(R.id.listViewSearch);
+
+                customListViewSearch = new CustomListViewSearch(getContext(), searchList);
+                listView.setAdapter(customListViewSearch);
+
+            }
+        });
 
         return view;
     }
